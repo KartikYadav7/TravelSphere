586521255f1cea6d58ae420436c363b9f5d4b815
kartikyadav7 <raokartik7503@gmail.com>
2026-03-31 10:04:30 +0530

    model work?

--- frontend/src/Components/UpcomingPackages/AIModel.jsx
+++ frontend/src/Components/UpcomingPackages/AIModel.jsx
@@ -4,8 +4,9 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.0-pro",
   }); 
+  
   
   const generationConfig = {
     temperature: 1,
